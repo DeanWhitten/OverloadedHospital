f8df493e01abbfe449c01c8b432c853e6cda33fb
Dean Whitten <36218923+DeanWhitten@users.noreply.github.com>
2021-04-07 23:20:44 -0400

    Removal of some functions to simplify and a condition inserted in the main to replace the removed function's purpose.

--- main.cpp
+++ main.cpp
@@ -28,110 +28,109 @@ using namespace std;
 
 // function for asking # of days. Also checks to make sure the number is positive and at least 1 day.
 int numDays(int d){
-    cout << "How many days was the stay?" << endl;
-    cin >> d;
-    if (d < 1 ){
-        cout << "ERROR: NUMBER BUST BE A POSITIVE NUMBER" << endl;
-        cout << "How many days was the stay?" << endl;
-        cin >> d;
-    } else{
-        return d;
-    }
-    return d;
+	cout << "How many days was the stay?" << endl;
+	cin >> d;
+	if (d < 1 ){
+		cout << "ERROR: NUMBER BUST BE A POSITIVE NUMBER" << endl;
+		cout << "How many days was the stay?" << endl;
+		cin >> d;
+	} else{
+		return d;
+	}
+	return d;
 }
 
 // function for asking the daily rate of a visit. Also checks to make sure a number is positive.
 double dailyRate (double dR){
-    cout << "What was he daily rate?" << endl;
-    cin >> dR;
-    if ( dR < 0){
-        cout << "ERROR: NUMBER MUST BE A POSITIVE NUMBER" << endl;
-        cout << "What was he daily rate?" << endl;
-        cin >> dR;
-    } else {
-        return dR;
-    }
-    return dR;
+	cout << "What was he daily rate?" << endl;
+	cin >> dR;
+	if ( dR < 0){
+		cout << "ERROR: NUMBER MUST BE A POSITIVE NUMBER" << endl;
+		cout << "What was he daily rate?" << endl;
+		cin >> dR;
+	} else {
+		return dR;
+	}
+	return dR;
 }
 
 // Function for asking the cost of the medication. Also checks to make sure a number is positive.
 double medCost(double m){
-    cout << "What was the cost of the medications?" << endl;
-    cin >> m;
-    if (m < 0){
-        cout << "ERROR: NUMBER MUST BE A POSITIVE NUMBER" << endl;
-        cout << "What was the cost of the medications?" << endl;
-        cin >> m;
-    } else {
-        return m;
-    }
-    return m;
+	cout << "What was the cost of the medications?" << endl;
+	cin >> m;
+	if (m < 0){
+		cout << "ERROR: NUMBER MUST BE A POSITIVE NUMBER" << endl;
+		cout << "What was the cost of the medications?" << endl;
+		cin >> m;
+	} else {
+		return m;
+	}
+	return m;
 }
 
 // Function for asking the cost of the medication. Also checks to make sure a number is positive.
 double serviceCost( double s){
-    cout << "What was the total cost of the services received? i.e x-rays, lab work etc." << endl;
-    cin >> s;
-    if ( s < 0) {
-        cout << "ERROR: NUMBER MUST BE A POSITIVE NUMBER" << endl;
-        cout << "What was the total cost of the services received? i.e x-rays, lab work etc." << endl;
-        cin >> s;
-    } else {
-        return s;
-    }
-    return s;
+	cout << "What was the total cost of the services received? i.e x-rays, lab work etc." << endl;
+	cin >> s;
+	if ( s < 0) {
+		cout << "ERROR: NUMBER MUST BE A POSITIVE NUMBER" << endl;
+		cout << "What was the total cost of the services received? i.e x-rays, lab work etc." << endl;
+		cin >> s;
+	} else {
+		return s;
+	}
+	return s;
 }
 
 //Function that calculates the total cost for an inpatient.
 double inpatientSum(int d, double dR, double m, double s){
-    double total = (d * dR) + m + s;
-    return total;
+	double total = (d * dR) + m + s;
+	return total;
 }
 
 //Function that calculates the total cost for an outpatient.
 double outpatientSum( double s, double m){
-    double total = s + m;
-    return total;
+	double total = s + m;
+	return total;
 }
-// Function that calls the other necessary functions for an inpatient billing.
-int inpatient(){
-    int days = numDays(0);
-    double rate = dailyRate(0);
-    double meds = medCost(0);
-    double services = serviceCost(0);
-    double sum = inpatientSum(days, rate, meds, services);
-
-    cout << days << "  " << rate << " " << meds << " " << services << " " << sum << endl;  // temp input checker/ function testing
-    return 0;
-}
-
-// Function that calls the other necessary functions for an Outpatient billing.
-int outpatient(){
-    double services = serviceCost(0);
-    double meds = medCost(0);
-    double sum = outpatientSum(services, meds);
-
-    cout << services<< " " << meds << " " << sum << endl;  // temp input checker/ function testing
-    return 0;
-}
-
 
 
 int main() {
-    string answer;
+	string answer;
+	int days;
+	double rate, meds, services, sum;
+	bool inpatient;
 
 
-    cout << "Was patient admitted as an inpatient or an outpatient?" << endl;
-    cin >> answer;
+	cout << "Was patient admitted as an inpatient or an outpatient?" << endl;
+	cin >> answer;
 
-    if (answer == "inpatient"){
-        inpatient();
-    } else if ( answer == "outpatient"){
-        outpatient();
-    } else {
-        cout << "Sorry " << answer << " is an invalid input" << endl;
-        main();
-    }
+	// checks if the value entered was inpatient or out patient and sets inpatient to true or false, if an improper value is entered it asks again by restarting program.
+	if (answer == "inpatient"){
+		inpatient = true;
+	} else if (answer == "outpatient"){
+		inpatient = false;
+	} else {
+		cout << "Sorry " << answer << " is an invalid input" << endl;
+		main();
+	}
 
-    return 0;
+	//Checks inpatient value and calls functions that executes questions related to patient status (inpatient or out patient)
+	if (inpatient == true){
+        days = numDays(0);
+        rate = dailyRate(0);
+        meds = medCost(0);
+        services = serviceCost(0);
+        sum = inpatientSum(days, rate, meds, services);
+
+        cout << days << "  " << rate << " " << meds << " " << services << " " << sum << endl;
+    } else if ( inpatient == false){
+        services = serviceCost(0);
+        meds = medCost(0);
+        sum = outpatientSum(services, meds);
+
+        cout << services<< " " << meds << " " << sum << endl;
+	}
+
+	return 0;
 }
